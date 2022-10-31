2128587cba0196506a117cf8936c4be774b00d42
Oliver KEB <oliver.keb@icloud.com>
2022-10-31 13:43:12 +0100

    ✨ feat(slug): create slug

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,26 @@
 class Question < ApplicationRecord
-    attribute :uuid, MySQLBinUUID::Type.new
-    before_create { self.uuid = ApplicationRecord.generate_uuid }
-    belongs_to :account
-    # validates :body, presence: true
+  attribute :uuid, MySQLBinUUID::Type.new
+  before_create { self.uuid = ApplicationRecord.generate_uuid }
+  belongs_to :question, class_name: "Question", optional: true
+  belongs_to :account
+
+  after_create :create_slug
+  before_create :update_slug
+
+  def to_param
+    slug
+  end 
+
+  private
+
+  def create_slug
+    update_slug
+    save!
+  end
+
+  def update_slug
+    self.slug = [title&.parameterize, uuid].compact.join("-")
+  end
+
+
 end
