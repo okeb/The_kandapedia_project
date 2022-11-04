74a71f269b12b0dca7fa0d41d3aefbc773162bbe
Oliver KEB <oliver.keb@icloud.com>
2022-11-04 15:51:51 +0100

    🐞 fix(avatar): fix show avatar

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,13 +1,14 @@
 class Profile < ApplicationRecord
   belongs_to :account
   has_one_attached :avatar
+  after_create :add_color
 
   def avatar_thumb
-    self.avatar.variant(resize_to_limit: [100, 100])
+    self.avatar.variant(resize_to_limit: [100, 100]).processed
   end
   
   def avatar_profile
-    self.avatar.variant(resize_to_limit: [250, 250])
+    self.avatar.variant(resize_to_limit: [250, 250]).processed
   end
 
   def get_initial_profile
@@ -17,5 +18,9 @@ class Profile < ApplicationRecord
     long_name.each{ |x|  initials += x[0]}
 
     initials[0..2].upcase
+  end
+
+  def add_color
+    self.color = Random.bytes(3).unpack1('H*').paint.brighten(23) 
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_04_135454) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_04_135926) do
   create_table "account_email_auth_keys", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "key", null: false
     t.datetime "deadline", null: false
@@ -95,6 +95,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_04_135454) do
     t.bigint "account_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "color"
     t.index ["account_id"], name: "index_profiles_on_account_id"
   end
 
