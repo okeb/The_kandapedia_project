86508bd98a8226adb349ab133cf5313e82869ca5
Oliver KEB <oliver.keb@icloud.com>
2022-11-06 23:35:06 +0100

    ✨ feat(questions):
    permit to add tag in the show edit page

--- app/javascript/controllers/select_tag_controller.js
+++ app/javascript/controllers/select_tag_controller.js
@@ -3,27 +3,38 @@ import TomSelect from "tom-select";
 
 // Connects to data-controller="select-tag"
 export default class extends Controller {
-  static targets = ["select"]
+  static targets = ["select", "input"]
   connect() {
-    new TomSelect(this.selectTarget,{
-      plugins: ['remove_button'],
+    let select_form = this.selectTarget.form;
+    let input_form = this.inputTarget;
+    new TomSelect(this.selectTarget, {
+      plugins: ["remove_button", "caret_position"],
       placeholder: "Ecrivez vos tags...",
       create: true,
-      onItemAdd:function(){
-        this.setTextboxValue('');
+      onChange(){
+        this.setTextboxValue("");
         this.refreshOptions();
+        input_form.value = this.getValue().join(", ");
+        select_form.requestSubmit();
       },
-      render:{
-        option:function(data,escape){
-          return '<div class="d-flex"><span>' + escape(data.value) + '</span><span class="ms-auto text-muted">' + escape(data.date) + '</span></div>';
+      persist: false,
+      render: {
+        option: function (data, escape) {
+          return (
+            '<div class="d-flex"><span>' +
+            escape(data.value) +
+            '</span><span class="ms-auto text-muted">' +
+            escape(data.date) +
+            "</span></div>"
+          );
         },
-        item:function(data,escape){
-          return '<div>' + escape(data.value) + '</div>';
+        item: function (data, escape) {
+          return "<div>" + escape(data.value) + "</div>";
         },
-        loading:function(data,escape){
+        loading: function (data, escape) {
           return '<div class="spinner"></div>';
         },
-      }
+      },
     });
   }
 }
