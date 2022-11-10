0e356cf320c904cae21838241807145d82064c7a
Oliver KEB <oliver.keb@icloud.com>
2022-11-10 18:29:21 +0100

    ✨ feat(profile):
    add the verification to the profile edit page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,21 @@
 class ProfilesController < ApplicationController
+  before_action :get_user_authenticate
   before_action :charge_profile
   before_action -> { rodauth.require_authentication }, except: [ :show, :avatar_thumb, :charge_profile, :profile_params ]
-  before_action :its_me, except: [ :show, :update, :avatar_thumb, :charge_profile, :profile_params ]
 
   ActsAsTaggableOn.remove_unused_tags = true
   ActsAsTaggableOn.force_lowercase = true
+
+  def pundit_user
+    current_account
+  end
   
   def show
     @questions = Question.all.where(account_id: @profile.account_id)
   end
 
   def edit
+    authorize @profile
   end
 
   def update
@@ -31,10 +36,10 @@ class ProfilesController < ApplicationController
     @profile = Profile.find_by(id: params[:id])
   end
 
-  def its_me
-    unless rodauth.session_value === @profile.account_id
-      redirect_to @profile, notice: "vous n'êtes pas en mesure de modifier ce compte",status: 403
-    end
+  def get_user_authenticate
+      if rodauth.logged_in?
+        @current_user = current_account
+      end
   end
 
   def profile_params
