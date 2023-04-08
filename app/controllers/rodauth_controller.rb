952c0b6b47be381e4b93a18dd683e687f3ee8d99
Oliver KEB <oliver.keb@icloud.com>
2023-04-08 17:39:41 +0200

    🦄 refactor(login):
    change the redirect action to skip login page if user is logged

--- app/controllers/rodauth_controller.rb
+++ app/controllers/rodauth_controller.rb
@@ -1,16 +1,4 @@
 class RodauthController < ApplicationController
   # used by Rodauth for rendering views, CSRF protection, and running any
   # registered action callbacks and rescue_from handlers
-
-  before_action :redirect_if_logged_in
-
-
-  private
-
-  def redirect_if_logged_in
-    if rodauth.logged_in? && request.path == rodauth.login_path
-      flash[:notice] = "Vous êtes déjà connecté"
-      redirect_to root_path # Remplacez dashboard_path par l'URL de la page de destination
-    end
-  end
 end
