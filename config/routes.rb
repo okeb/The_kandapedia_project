e6dcf2ba113304c1d063ed00ede20f5face2c5ef
Oliver KEB <oliver.keb@icloud.com>
2023-04-14 18:34:05 +0200

    :sparkles: feat(kandies):
    - add candies in db
    - generated a migration and launched migration
    - add
    routes for kandies ressource
    - create view (index and show) pages for
    kandies
    - create controller for kandies
    - add view component to the gem
    files
    - create a kandyIndex to display kandies in index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :candies
   resources :questions
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
@@ -12,26 +13,26 @@ Rails.application.routes.draw do
     # end
     resources :profiles
 
-    post "/profiles/:id/follow", to: "profiles#follow_profile", as: "follow_profile"
-    post "/profiles/:id/unfollow", to: "profiles#unfollow_profile", as: "unfollow_profile"
-    post "/profiles/:id/blocking", to: "profiles#blocking_profile", as: "blocking_profile"
-    post "/profiles/:id/unblocking", to: "profiles#unblocking_profile", as: "unblocking_profile"
-    post "/profiles/:id/accept", to: "profiles#accept_following", as: "accept_following"
-    post "/profiles/:id/decline", to: "profiles#decline_following", as: "decline_following"
-    post "/profiles/:id/cancel", to: "profiles#cancel_following", as: "cancel_following"
+    post '/profiles/:id/follow', to: 'profiles#follow_profile', as: 'follow_profile'
+    post '/profiles/:id/unfollow', to: 'profiles#unfollow_profile', as: 'unfollow_profile'
+    post '/profiles/:id/blocking', to: 'profiles#blocking_profile', as: 'blocking_profile'
+    post '/profiles/:id/unblocking', to: 'profiles#unblocking_profile', as: 'unblocking_profile'
+    post '/profiles/:id/accept', to: 'profiles#accept_following', as: 'accept_following'
+    post '/profiles/:id/decline', to: 'profiles#decline_following', as: 'decline_following'
+    post '/profiles/:id/cancel', to: 'profiles#cancel_following', as: 'cancel_following'
 
     resources :questions do
-      put "add_to_readlist", to: 'questions#add_to_readlist'
-      put "remove_to_readlist", to: 'questions#remove_to_readlist'
-      put "bookmark", to: 'questions#toggle_to_bookmark'
-      patch "update_tags", to: 'questions#update_tags'
+      put 'add_to_readlist', to: 'questions#add_to_readlist'
+      put 'remove_to_readlist', to: 'questions#remove_to_readlist'
+      put 'bookmark', to: 'questions#toggle_to_bookmark'
+      patch 'update_tags', to: 'questions#update_tags'
 
-      put "awesome_question", to: 'questions#add_awesome'
-      put "perfect_question", to: 'questions#add_perfect'
-      put "nice_question", to: 'questions#add_nice'
-      put "wrong_question", to: 'questions#add_wrong'
-      put "bad_question", to: 'questions#add_bad'
+      put 'awesome_question', to: 'questions#add_awesome'
+      put 'perfect_question', to: 'questions#add_perfect'
+      put 'nice_question', to: 'questions#add_nice'
+      put 'wrong_question', to: 'questions#add_wrong'
+      put 'bad_question', to: 'questions#add_bad'
     end
   end
-    
+
 end
